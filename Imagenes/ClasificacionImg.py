e0593c81a24e7cd2b45f90b809c7731b6a938a19
isaacGGomez <isaacggf@hotmail.com>
2023-04-09 13:41:35 -0600

    Tarea Img

--- Imagenes/ClasificacionImg.py
+++ Imagenes/ClasificacionImg.py
@@ -1,19 +1,61 @@
-from PIL import Image as IM
-import numpy as np
+from pytictoc import TicToc
+t = TicToc() #create instance of class
 from Imagenes.KohonenFunc import *
 #%%
 img1 = IM.open('imagen1Deutschland-1.jpg')
-
-#%%
 img1Arr = np.array(img1)[:,:,:3]
 img1ArrS = img1Arr.shape
 img1Arr = np.reshape(img1Arr,(img1ArrS[0]*img1ArrS[1],-1))
 img1Arr = np.append(img1Arr,np.zeros((img1Arr.__len__(),1)),axis = 1)
 #%%
-img1mean = IM.fromarray(np.uint8(img1ArrB))
+t.tic() #Start timer
+centroid = kmeans(img1Arr,3,3)
+t.toc() #Time elapsed since t.tic()
 #%%
-img1mean.save('Test.png')
+img1Arr = img1Arr.astype(int)
+#%%
+for i in range(len(img1Arr)):
+    img1Arr[i,0] = centroid[img1Arr[i,3]-1,0]
+    img1Arr[i,1] = centroid[img1Arr[i,3]-1,1]
+    img1Arr[i,2] = centroid[img1Arr[i,3]-1,2]
+img1Arr=np.reshape(img1Arr[:,:3],(img1ArrS[0],img1ArrS[1],3))
+#%%
+img1mean = IM.fromarray(np.uint8(img1Arr))
+#%%
+img1mean.save('KmeansTest3.png')
+
+#%%
+img2 = IM.open('imagen2México.jpg')
+t.tic()
+crearimg(img2)
+t.toc()
+#%%
+img3 = IM.open('imagen3Gandhi.jpg')
+t.tic()
+crearimg(img3)
+t.toc()
+#%%
+img4 = IM.open('imagen4Lauterbrunnen.jpg')
+iteration = [26]
+t.tic()
+crearimg(img=img4,vector=iteration)
+t.toc()
+#%%
+#for h in [2,3,10]:
+#    img1Arr = np.array(img1)[:,:,:3]
+#    img1ArrS = img1Arr.shape
+#    img1Arr = np.reshape(img1Arr,(img1ArrS[0]*img1ArrS[1],-1))
+#    img1Arr = np.append(img1Arr,np.zeros((img1Arr.__len__(),1)),axis = 1)
+#    centroid = centroides(img1Arr,h,3)
+#    img1Arr = img1Arr.astype(int)
+#    for i in range(len(img1Arr)):
+#        img1Arr[i,0] = centroid[img1Arr[i,3]-1,0]
+#        img1Arr[i,1] = centroid[img1Arr[i,3]-1,1]
+#        img1Arr[i,2] = centroid[img1Arr[i,3]-1,2]
+#    img1Arr=np.reshape(img1Arr[:,:3],(img1ArrS[0],img1ArrS[1],3))
+#    img1mean = IM.fromarray(np.uint8(img1Arr))
+#    var = 'Test2'+str(h)+'.png'
+#    img1mean.save(var)
+
 #%%
 
-img1Arr = np.append(img1Arr,np.zeros((img1Arr.__len__(),1)),axis = 1)
-df = np.append(df,np.zeros((df.__len__(),1)),axis=1)
--- Imagenes/KohonenFunc.py
+++ Imagenes/KohonenFunc.py
@@ -1,9 +1,9 @@
 import numpy  as np
-import pandas as pd
+from PIL import Image as IM
 
 def centroides(df, clases, caracteristicas):
-    tolerancia = .1
-    error = 99999
+    tolerancia = 1
+    error = 500
     distancias = []
     n_clases = int(clases)
     j = 2
@@ -22,6 +22,44 @@ def centroides(df, clases, caracteristicas):
         j +=1
 
     return centroids
-def asignar( entrada, centroids ):
-    return np.argmin( np.sum( (entrada - centroids) ** 2, axis=1) ) + 1
 
+def asignar_a_clase ( entrada, centroides ):
+    return np.argmin(np.sum( (entrada - centroides) ** 2, axis=1)) + 1
+
+def kmeans(data, n_clases, n_caracteristicas):
+    n_clases = int(n_clases)
+    if n_clases < 2:
+        return None
+    n_obs, _ = data.shape
+    #data = np.append(data, np.zeros([n_obs,1]), axis=1)
+    centroides = np.random.random([n_clases, n_caracteristicas]) * (abs(np.min(data[:,:-1], axis = 0) - np.max(data[:,:-1], axis = 0))).T + np.min( data[:,:-1], axis = 0)
+    prev_centroides = None
+    while np.not_equal(prev_centroides, centroides).any() :
+        for i in range(n_obs):
+            data[i, n_caracteristicas] = asignar_a_clase( data[i,:-1] , centroides )
+        prev_centroides = centroides
+        for i in range(n_clases):
+            tmp = data[np.where(data[:,-1] == i+1)]
+            #print(tmp)
+            centroides[i] = (np.mean(tmp[:,0]), np.mean(tmp[:,1]),np.mean(tmp[:,2]))
+    return centroides
+
+
+#%%
+def crearimg(img,vector):
+    vec = vector
+    for h in vec:
+        img1Arr = np.array(img)[:,:,:3]
+        img1ArrS = img1Arr.shape
+        img1Arr = np.reshape(img1Arr,(img1ArrS[0]*img1ArrS[1],-1))
+        img1Arr = np.append(img1Arr,np.zeros((img1Arr.__len__(),1)),axis = 1)
+        centroid = centroides(img1Arr,h,3)
+        img1Arr = img1Arr.astype(int)
+        for i in range(len(img1Arr)):
+            img1Arr[i,0] = centroid[img1Arr[i,3]-1,0]
+            img1Arr[i,1] = centroid[img1Arr[i,3]-1,1]
+            img1Arr[i,2] = centroid[img1Arr[i,3]-1,2]
+        img1Arr=np.reshape(img1Arr[:,:3],(img1ArrS[0],img1ArrS[1],3))
+        img1mean = IM.fromarray(np.uint8(img1Arr))
+        var = 'Prueba'+str(h)+'.png'
+        img1mean.save(var)
